fd319d5c19a161aa12e59cd5c1c37c28f9749f76
Lingmiao Qiu <qiu.lin@husky.neu.edu>
2019-09-27 18:05:10 -0400

    The straightforward version of memory game

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -3,75 +3,104 @@ import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
 export default function game_init(root) {
-  ReactDOM.render(<Starter />, root);
+    ReactDOM.render(<Starter />, root);
 }
 
 class Starter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { left: false };
-  }
-
-  swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
-    this.setState(state1);
-  }
-
-  hax(_ev) {
-    alert("hax!");
-  }
-
-  render() {
-    /* let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
-
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
-
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            // left: false
+            board: [["A", "B", "C", "D"], ["E", "F", "G", "H"], ["A", "B", "C", "D"], ["E", "F", "G", "H"]],
+            status: [["", "", "", ""], ["", "", "", ""], ["", "", "", ""], ["", "", "", ""]],
+            count: 0,
+            prev_i: -1,
+            prev_j: -1,
+            curr_i: -1,
+            curr_j: -1
+        };
     }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
-    } */
 
-    // The start of real code and the end of the starter code
-      let blank = <div className="column">
-          <p>Nothing here.</p>
-      </div>;
 
-    return (<div className="grid-container">
+    handleClick(i, j) {
+        if (this.state.count === 2) {
+            console.log("reset")
+            let empty_board = this.state.status
 
-            <div className="grid-item" onClick={this.hax.bind(this)}>1</div>
-            <div className="grid-item">2</div>
-            <div className="grid-item">3</div>
-            <div className="grid-item">4</div>
+            if (this.state.status[this.state.curr_i][this.state.curr_j] !== this.state.status[this.state.prev_i][this.state.prev_j]) {
+                empty_board[this.state.curr_i][this.state.curr_j] = ""
+                empty_board[this.state.prev_i][this.state.prev_j] = ""
+            }
+            empty_board[i][j] = this.state.board[i][j]
+            this.setState({board: this.state.board, status: empty_board, count: 1, prev_i: -1, prev_j: -1,
+                curr_i: i, curr_j: j})
+            console.log(this.state.status)
+        } else {
+            let tmp_status = this.state.status
+            tmp_status[i][j] = this.state.board[i][j]
+            this.setState({board: this.state.board, status: tmp_status, count: this.state.count + 1,
+                prev_i: this.state.curr_i, prev_j: this.state.curr_j, curr_i: i, curr_j: j})
+            console.log(this.state.status)
+        }
+    }
 
-            <div className="grid-item">1</div>
-            <div className="grid-item">2</div>
-            <div className="grid-item">3</div>
-            <div className="grid-item">4</div>
 
-            <div className="grid-item">1</div>
-            <div className="grid-item">2</div>
-            <div className="grid-item">3</div>
-            <div className="grid-item">4</div>
+    renderSquare(i, j) {
+        return <Square value= {this.state.status[i][j]} onClick={() => this.handleClick(i, j)} />
+    }
 
-            <div className="grid-item">1</div>
-            <div className="grid-item">2</div>
-            <div className="grid-item">3</div>
-            <div className="grid-item">4</div>
-    </div>
-    )
+    render() {
+        return (
+            <div>
+                <h2><b>Welcome to Memory Game</b></h2>
+                <div className="game">
+                    <div className="game-board">
+                        <Board root={this} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
-  }
+class Board extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.props.root.renderSquare(0, 0)}
+                    {this.props.root.renderSquare(0, 1)}
+                    {this.props.root.renderSquare(0, 2)}
+                    {this.props.root.renderSquare(0, 3)}
+                </div>
+                <div className="board-row">
+                    {this.props.root.renderSquare(1, 0)}
+                    {this.props.root.renderSquare(1, 1)}
+                    {this.props.root.renderSquare(1, 2)}
+                    {this.props.root.renderSquare(1, 3)}
+                </div>
+                <div className="board-row">
+                    {this.props.root.renderSquare(2, 0)}
+                    {this.props.root.renderSquare(2, 1)}
+                    {this.props.root.renderSquare(2, 2)}
+                    {this.props.root.renderSquare(2, 3)}
+                </div>
+                <div className="board-row">
+                    {this.props.root.renderSquare(3, 0)}
+                    {this.props.root.renderSquare(3, 1)}
+                    {this.props.root.renderSquare(3, 2)}
+                    {this.props.root.renderSquare(3, 3)}
+                </div>
+            </div>
+        )
+    }
+}
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
